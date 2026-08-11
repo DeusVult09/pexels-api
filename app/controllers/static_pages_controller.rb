552209f24990797d3bd25a7bd6996bc5adca7881
Zarifa <mamadalievazara@gmail.com>
2026-08-12 00:42:11 +0200

    Build Static Pages controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
+    @collection_id = params[:collection_id]
+
+    if @collection_id.blank?
+      flash.now[:alert] = "Please, enter your collection ID"
+    else
+      @photos = PexelsClient.new.my_photos(@collection_id) 
+    end
   end
 end
--- app/services/pexels_client.rb
+++ app/services/pexels_client.rb
@@ -1,3 +1,20 @@
 class PexelsClient
-  
+  BASE_URL = "https://api.pexels.com"
+
+  def my_photos(collection_id)
+    response = HTTParty.get(
+      "#{BASE_URL}/v1/collections/#{collection_id}",
+      headers: {
+        'Authorization' => Rails.application.credentials.pexels.api_key   
+      },
+      query: {
+        type: 'photos'
+      }
+    )
+    data = response.parsed_response
+     
+    if data['media'].empty?
+      puts "This media collection is empty"
+    end
+  end
 end
